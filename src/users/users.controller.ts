7fe772eecd62c3ebc3012095495609cda541c097
AnisurRahman06046 <anisurrahman06046@gmail.com>
2024-06-15 04:08:16 +0600

    done

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dtos/user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private userService: UsersService) {}
+
+  //   register user
+  @Post('register')
+  async register(@Body() payload: CreateUserDto) {
+    console.log(payload);
+    const result = await this.userService.register(payload);
+    return {
+      status: HttpStatus.CREATED,
+      message: 'Todo is added',
+      data: result,
+    };
+  }
+}
