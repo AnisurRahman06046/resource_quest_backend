c8abd27faa6288cfd4772fe5625776a037514c31
AnisurRahman06046 <anisurrahman06046@gmail.com>
2024-06-15 16:03:46 +0600

    model is configured

--- src/resources/resources.module.ts
+++ src/resources/resources.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ResourcesController } from './resources.controller';
 import { ResourcesService } from './resources.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Resource, resourceSchema } from 'src/schemas/resource.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Resource.name, schema: resourceSchema },
+    ]),
+  ],
   controllers: [ResourcesController],
   providers: [ResourcesService],
 })
--- src/resources/resources.service.ts
+++ src/resources/resources.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Resource } from 'src/schemas/resource.schema';
 
 @Injectable()
-export class ResourcesService {}
+export class ResourcesService {
+  constructor(
+    @InjectModel(Resource.name) private resourceModel: Model<Resource>,
+  ) {}
+}
