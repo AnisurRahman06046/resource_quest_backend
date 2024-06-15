a7973e51c97d348928b43e61177a05fffbdd579e
AnisurRahman06046 <anisurrahman06046@gmail.com>
2024-06-15 17:31:12 +0600

    get all resources by user done

--- src/resources/resources.service.ts
+++ src/resources/resources.service.ts
@@ -1,6 +1,6 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
 import { Resource } from 'src/schemas/resource.schema';
 import { CreateResourceDto } from './dtos/resource.dto';
 
@@ -15,5 +15,13 @@ export class ResourcesService {
     const result = await this.resourceModel.create(payload);
     if (!result)
       throw new HttpException('Failed to add', HttpStatus.BAD_REQUEST);
+    return result;
+  }
+  //   get all the resources by user
+  async resourcesByUser(id: string) {
+    const resources = await this.resourceModel.find({ user: id });
+    if (!resources || resources.length === 0)
+      throw new HttpException('no resource found', HttpStatus.NOT_FOUND);
+    return resources;
   }
 }
