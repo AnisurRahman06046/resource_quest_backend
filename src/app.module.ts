cfe95dfe2c60f6ccee79d849cabc2845c911a887
AnisurRahman06046 <anisurrahman06046@gmail.com>
2024-06-15 15:56:10 +0600

    resource schema is added

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { JwtModule } from '@nestjs/jwt';
+import { ResourcesModule } from './resources/resources.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { JwtModule } from '@nestjs/jwt';
       signOptions: { expiresIn: '1m' },
     }),
     UsersModule,
+    ResourcesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/schemas/resource.schema.ts
@@ -0,0 +1,17 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
+import { User } from './user.schema';
+
+@Schema({ timestamps: true })
+export class Resource {
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User' })
+  user: User;
+
+  @Prop({ required: [true, 'Title is required'] })
+  title: string;
+
+  @Prop({ required: [true, 'Link is required'] })
+  link: string;
+}
+
+export const resourceSchema = SchemaFactory.createForClass(Resource);
